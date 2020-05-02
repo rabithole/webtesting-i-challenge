978bf2eba6b75d4452b60c3050119f3ae20e8c4a
Robert Kulp <pedalinon@gmail.com>
2020-05-01 17:16:03 -0700

    test

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -2,33 +2,33 @@ const { hello, succeed, fail, repair, get, sum } = require('./enhancer.js');
 // test away!
 
 describe('Enhancer.js File', () => {
-    // it('Hello World Test!', () => {
-    //     expect(hello()).toBe('Hello Fucking World!');
-    // });
+    it('Hello World Test!', () => {
+        expect(hello()).toBe('Hello Fucking World!');
+    });
 
-    // it('sum of two numbers', () => {
-    //     expect(sum(5,5)).toBe(10); 
-    // })
-
-    // it('repair process', () => {
-    //     expect(repair(player)).toHaveProperty('durability', 100);
-    //     expect(repair(player)).toHaveProperty('name');
-    // })
-
-    // it('Enhancement success!', () => {
-    //     const playEnhance = playerSuccess.enhancement;
-    //     const playerEnhanced = playEnhance + 1;
-
-    //     expect(succeed(playerSuccess)).toHaveProperty('enhancement', playerEnhanced);
-    // })
-
-    // it('Enhancement Fail', () => {        
-    //     expect(fail(playerFail)).toHaveProperty('enhancement', 0);
-    // })
-
-    it('Modify name with enhancement level', () => {
-        expect(get(playerGet)).toContain('[');
+    it('sum of two numbers', () => {
+        expect(sum(5,5)).toBe(10); 
     })
+
+    it('repair process', () => {
+        expect(repair(player)).toHaveProperty('durability', 100);
+        expect(repair(player)).toHaveProperty('name');
+    })
+
+    it('Enhancement success!', () => {
+        const playEnhance = playerSuccess.enhancement;
+        const playerEnhanced = playEnhance + 1;
+
+        expect(succeed(playerSuccess)).toHaveProperty('enhancement', playerEnhanced);
+    })
+
+    it('Enhancement Fail', () => {        
+        expect(fail(playerFail)).toHaveProperty('enhancement', 0);
+    })
+
+    // it('Modify name with enhancement level', () => {
+    //     expect(get(playerGet)).toContain('[');
+    // })
 });
 
 const player = {
@@ -40,7 +40,7 @@ const player = {
 const playerGet = {
     name: 'Joe',
     durability: 55, // Number from 0 to 100;
-    enhancement: 1 // Number from 0 to 20
+    enhancement: 2 // Number from 0 to 20
 }
 
 const playerSuccess = {
